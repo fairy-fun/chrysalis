c8fe6e78c8f0d854a5127181fa3cdafb92940b47
fairy-fun <github@makeyourfairytale.com>
2026-04-20 15:39:46 -0400

    Update repo_contract.php

--- private/framework/contracts/repo_contract.php
+++ private/framework/contracts/repo_contract.php
@@ -2,3 +2,10 @@
 declare(strict_types=1);
 
 const FW_AUDIT_ENTRYPOINT = 'private/framework/contracts/chrysalis_hydration_prompt.md';
+
+const FW_REPO_CONTRACT = [
+    'audit_entrypoint' => FW_AUDIT_ENTRYPOINT,
+    'doctrine_owner' => 'php',
+    'db_role' => 'transport_only',
+    'ci_role' => 'enforcement_authority',
+];
