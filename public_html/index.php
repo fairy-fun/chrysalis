ca04e8b680346d2637a67cf427fe4f3df121fa96
fairy-fun <github@makeyourfairytale.com>
2026-04-21 01:38:44 -0400

    guard files

--- public_html/index.php
+++ public_html/index.php
@@ -5,4 +5,4 @@ http_response_code(404);
 header('Content-Type: text/plain; charset=utf-8');
 
 echo "Not Found\n";
-echo "/pecherie/ is not a public entrypoint.\n";
+echo "No public site is available at this address.\n";
