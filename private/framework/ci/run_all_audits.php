3df9dd5ba8c583e896a8afb6f0ed19656edc5754
fairy-fun <github@makeyourfairytale.com>
2026-04-24 12:41:54 -0400

    audit for domain mirror

--- private/framework/ci/run_all_audits.php
+++ private/framework/ci/run_all_audits.php
@@ -15,6 +15,7 @@ require __DIR__ . '/../audit/audit_figure_entity_mirror.php';
 require __DIR__ . '/../audit/audit_typed_entity_reference_integrity.php';
 require __DIR__ . '/../audit/audit_untyped_varchar_id_surface.php';
 require __DIR__ . '/../audit/audit_expression_domain_alias.php';
+require __DIR__ . '/../audit/audit_domain_entity_mirror.php';
 
 $pdo = makePdo();
 $schemaName = verifyExpectedDatabase($pdo);
@@ -32,6 +33,7 @@ $audits = [
     'typed entity reference integrity' => fn () => assert_typed_entity_reference_integrity($pdo, $schemaName),
     'untyped varchar id surface' => fn () => assert_untyped_varchar_id_surface($pdo, $schemaName),
     'expression domain alias' => fn () => assert_expression_domain_alias($pdo, $schemaName),
+    'domain entity mirror' => fn () => assert_domain_entity_mirror($pdo, $schemaName),
 ];
 
 foreach ($audits as $auditName => $runAudit) {
