6177d81d5796bac980eca039548ff4e3fc19a2cb
fairy-fun <github@makeyourfairytale.com>
2026-04-27 23:45:18 -0400

    Then the suggester should return proposals only:

--- private/framework/expression/entity_event_theme_link_suggester.php
+++ private/framework/expression/entity_event_theme_link_suggester.php
@@ -1,1 +1,33 @@
 <?php
+
+declare(strict_types=1);
+
+function suggest_entity_event_theme_link(PDO $pdo, ?string $contextId = null): array
+{
+    $contextId = is_string($contextId) && trim($contextId) !== ''
+        ? trim($contextId)
+        : null;
+
+    return [
+        'proposal_type' => 'entity_event_theme_link',
+        'context_id' => $contextId,
+
+        'subject_entity_id' => 'entity_event_123',
+        'fact_type_id' => 'fact_type_event_theme',
+        'object_entity_id' => 'entity_theme_sacrifice',
+
+        'match_status' => 'placeholder',
+
+        'proposed_action' => [
+            'table' => 'entity_linked_facts',
+            'operation' => 'insert_if_missing',
+            'conflict_check' => [
+                'subject_entity_id',
+                'fact_type_id',
+                'object_entity_id',
+            ],
+        ],
+
+        'sql_text' => null,
+    ];
+}
