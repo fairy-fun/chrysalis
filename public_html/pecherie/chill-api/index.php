329e3800be356f14bc101f94ecd3d263daaaf186
fairy-fun <github@makeyourfairytale.com>
2026-04-30 00:25:18 -0400

    json hang

--- public_html/pecherie/chill-api/index.php
+++ public_html/pecherie/chill-api/index.php
@@ -1,7 +1,14 @@
 <?php
 declare(strict_types=1);
 
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 header('Content-Type: application/json; charset=utf-8');
+
+echo json_encode(['hit' => 'index']);
+exit;
 
 function api_error(int $statusCode, string $message): never
 {
