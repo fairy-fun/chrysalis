4be40fd02b6812cdb1fad4c8bf3ffd437c10f7b9
fairy-fun <github@makeyourfairytale.com>
2026-05-10 22:53:49 -0400

    test_fact_resolver.php

---
+++ private/scripts/test_fact_resolver.php
@@ -0,0 +1,6 @@
+<?php
+require_once __DIR__ . '/../bootstrap.php';
+require_once __DIR__ . '/../framework/facts/apply_fact.php';
+require_once __DIR__ . '/../framework/facts/fact_resolver.php';
+
+$pdo = db();
