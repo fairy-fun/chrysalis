bdb3023b420e481003d97b35643cabcad768f1fa
fairy-fun <github@makeyourfairytale.com>
2026-05-17 22:58:46 -0400

    workflow_boolean_transition_driver.php

--- private/framework/procedures/workflow_boolean_transition_driver.php
+++ private/framework/procedures/workflow_boolean_transition_driver.php
@@ -1,37 +1,18 @@
 <?php
 declare(strict_types=1);
 
-function fw_dispatch_workflow_transition(
+function fw_resolve_boolean_workflow_transition(
     array $transition,
     bool $success,
     array $input = [],
     array $context = []
 ): ?string {
 
-    $driver = $transition['driver'] ?? 'boolean';
+    $definition = $transition['transition'] ?? $transition;
 
-    $drivers = [
-        'boolean' => 'fw_resolve_boolean_workflow_transition',
-    ];
-
-    if (!isset($drivers[$driver])) {
-        throw new RuntimeException(
-            "Unknown workflow transition driver: {$driver}"
-        );
+    if ($success) {
+        return $definition['next'] ?? null;
     }
 
-    $function = $drivers[$driver];
-
-    if (!function_exists($function)) {
-        throw new RuntimeException(
-            "Workflow transition driver function not loaded: {$function}"
-        );
-    }
-
-    return $function(
-        $transition,
-        $success,
-        $input,
-        $context
-    );
+    return $definition['failure_state'] ?? null;
 }
