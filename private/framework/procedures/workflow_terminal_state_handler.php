7d39b013c0e008e4a69978bc134396f8ff61046a
fairy-fun <github@makeyourfairytale.com>
2026-05-19 00:35:54 -0400

    workflow_terminal_state_handler.php

--- private/framework/procedures/workflow_terminal_state_handler.php
+++ private/framework/procedures/workflow_terminal_state_handler.php
@@ -1,6 +1,8 @@
 <?php
 declare(strict_types=1);
 
+require_once __DIR__ . '/workflow_value_resolver.php';
+
 function fw_handle_workflow_terminal_state(
     PDO $pdo,
     array $workflow,
@@ -10,10 +12,22 @@ function fw_handle_workflow_terminal_state(
     array $context = []
 ): array {
 
+    $response = null;
+
+    if (array_key_exists('response', $state)) {
+
+        $response = fw_resolve_workflow_value(
+            $state['response'],
+            $input,
+            $context
+        );
+    }
+
     return [
         'workflow_id' => $workflow['workflow_id'],
         'state' => $stateName,
         'type' => 'terminal',
         'message' => $state['message'] ?? null,
+        'response' => $response,
     ];
 }
