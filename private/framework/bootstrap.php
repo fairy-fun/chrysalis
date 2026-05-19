801b339c9f4c32d92849801b8b3f1fdfc59b9593
fairy-fun <github@makeyourfairytale.com>
2026-05-18 23:55:12 -0400

    bootstrap.php

--- private/framework/bootstrap.php
+++ private/framework/bootstrap.php
@@ -23,6 +23,7 @@ require_once __DIR__ . '/procedures/workflow_context_writer.php';
 
 require_once __DIR__ . '/procedures/workflow_boolean_transition_driver.php';
 require_once __DIR__ . '/procedures/workflow_match_transition_driver.php';
+require_once __DIR__ . '/procedures/workflow_transition_dispatcher.php';
 
 require_once __DIR__ . '/procedures/workflow_driver_dispatcher.php';
 require_once __DIR__ . '/procedures/workflow_db_driver.php';
