f9b5083f90a42a8fa68a3ed8b1a26598ef235773
fairy-fun <github@makeyourfairytale.com>
2026-05-22 02:30:04 -0400

    Use normalized Book chronology inputs in event workflow

--- private/framework/procedures/workflow_calendar_book_event_create_definition.php
+++ private/framework/procedures/workflow_calendar_book_event_create_definition.php
@@ -20,54 +20,69 @@ return [
                 'cases' => [
                     '' => 'terminal_missing_projection_id',
                 ],
-                'default' => 'validate_projection',
+                'default' => 'await_week_index',
             ],
         ],
 
-        'validate_projection' => [
+        'await_week_index' => [
+            'type' => 'input',
+            'prompt' => 'Which week should this event belong to? You can answer with a number or a label such as Week 1.',
+            'expected_input' => 'week_index',
+
+            'transition' => [
+                'driver' => 'match',
+                'value' => '$input.week_index',
+                'cases' => [
+                    '' => 'terminal_missing_week_index',
+                ],
+                'default' => 'await_day_index',
+            ],
+        ],
+
+        'await_day_index' => [
+            'type' => 'input',
+            'prompt' => 'Which day should this event belong to? You can answer with a day name, a number, or a label such as Day 2.',
+            'expected_input' => 'day_index',
+
+            'transition' => [
+                'driver' => 'match',
+                'value' => '$input.day_index',
+                'cases' => [
+                    '' => 'terminal_missing_day_index',
+                ],
+                'default' => 'await_time_index',
+            ],
+        ],
+
+        'await_time_index' => [
+            'type' => 'input',
+            'prompt' => 'Which time slot should this event belong to? You can answer with a number or a label such as Time 1.',
+            'expected_input' => 'time_index',
+
+            'transition' => [
+                'driver' => 'match',
+                'value' => '$input.time_index',
+                'cases' => [
+                    '' => 'terminal_missing_time_index',
+                ],
+                'default' => 'normalize_book_event_input',
+            ],
+        ],
+
+        'normalize_book_event_input' => [
             'type' => 'action',
 
             'action' => [
-                'driver' => 'db',
-                'operation' => 'select_one',
-                'store' => 'projection',
+                'driver' => 'calendar',
+                'operation' => 'normalize_book_event_input',
 
-                'sql' => '
-                    SELECT
-                        id,
-                        entity_id,
-                        projection_type_id,
-                        projection_code
-                    FROM calendar_projections
-                    WHERE projection_type_id = :required_projection_type_id
-                      AND (
-                          (
-                              :projection_id_numeric_probe REGEXP "^[0-9]+$"
-                              AND id = CAST(:projection_id_numeric_value AS UNSIGNED)
-                          )
-                          OR entity_id = :projection_id_entity_id
-                          OR projection_code = :projection_id_projection_code
-                          OR LOWER(REPLACE(REPLACE(projection_code, "_", ""), "-", ""))
-                             = LOWER(REPLACE(REPLACE(REPLACE(:projection_id_normalized_code, " ", ""), "_", ""), "-", ""))
-                          OR CONCAT("book", CAST(id AS CHAR))
-                             = LOWER(REPLACE(REPLACE(REPLACE(:projection_id_book_label, " ", ""), "_", ""), "-", ""))
-                      )
-                    ORDER BY id ASC
-                    LIMIT 1
-                ',
-
-                'bindings' => [
-                    'projection_id_numeric_probe' => '$input.projection_id',
-                    'projection_id_numeric_value' => '$input.projection_id',
-                    'projection_id_entity_id' => '$input.projection_id',
-                    'projection_id_projection_code' => '$input.projection_id',
-                    'projection_id_normalized_code' => '$input.projection_id',
-                    'projection_id_book_label' => '$input.projection_id',
-                    'required_projection_type_id' => '$context.required_projection_type_id',
+                'payload' => [
+                    'projection' => '$input.projection_id',
+                    'week' => '$input.week_index',
+                    'day' => '$input.day_index',
+                    'time' => '$input.time_index',
                 ],
             ],
-
-            'success_if' => 'row_exists',
 
             'transition' => [
                 'driver' => 'boolean',
@@ -87,23 +102,8 @@ return [
 
             'transition' => [
                 'driver' => 'boolean',
-                'next' => 'await_week_index',
+                'next' => 'validate_book_week',
                 'failure_state' => 'terminal_unsupported_projection_type',
-            ],
-        ],
-
-        'await_week_index' => [
-            'type' => 'input',
-            'prompt' => 'Which week should this event belong to? You can answer with a number or a label such as Week 1.',
-            'expected_input' => 'week_index',
-
-            'transition' => [
-                'driver' => 'match',
-                'value' => '$input.week_index',
-                'cases' => [
-                    '' => 'terminal_missing_week_index',
-                ],
-                'default' => 'validate_book_week',
             ],
         ],
 
@@ -123,27 +123,13 @@ return [
                         week_index
                     FROM calendar_book_weeks
                     WHERE projection_id = :projection_id
-                      AND week_index = CAST(
-                          REPLACE(
-                              REPLACE(
-                                  REPLACE(
-                                      LOWER(REPLACE(:week_index_input, "week", "")),
-                                      " ",
-                                      ""
-                                  ),
-                                  "_",
-                                  ""
-                              ),
-                              "-",
-                              ""
-                          ) AS UNSIGNED
-                      )
+                      AND week_index = :week_index
                     LIMIT 1
                 ',
 
                 'bindings' => [
-                    'projection_id' => '$context.projection.id',
-                    'week_index_input' => '$input.week_index',
+                    'projection_id' => '$context.calendar_normalized_input.projection_id',
+                    'week_index' => '$context.calendar_normalized_input.week_index',
                 ],
             ],
 
@@ -151,23 +137,8 @@ return [
 
             'transition' => [
                 'driver' => 'boolean',
-                'next' => 'await_day_index',
+                'next' => 'validate_book_day',
                 'failure_state' => 'terminal_book_week_not_found',
-            ],
-        ],
-
-        'await_day_index' => [
-            'type' => 'input',
-            'prompt' => 'Which day should this event belong to? You can answer with a day name, a number, or a label such as Day 2.',
-            'expected_input' => 'day_index',
-
-            'transition' => [
-                'driver' => 'match',
-                'value' => '$input.day_index',
-                'cases' => [
-                    '' => 'terminal_missing_day_index',
-                ],
-                'default' => 'validate_book_day',
             ],
         ],
 
@@ -190,44 +161,14 @@ return [
                     FROM calendar_book_days
                     WHERE projection_id = :projection_id
                       AND week_id = :week_id
-                      AND day_index = CASE
-                          WHEN LOWER(TRIM(:day_name_sunday)) = "sunday" THEN 1
-                          WHEN LOWER(TRIM(:day_name_monday)) = "monday" THEN 2
-                          WHEN LOWER(TRIM(:day_name_tuesday)) = "tuesday" THEN 3
-                          WHEN LOWER(TRIM(:day_name_wednesday)) = "wednesday" THEN 4
-                          WHEN LOWER(TRIM(:day_name_thursday)) = "thursday" THEN 5
-                          WHEN LOWER(TRIM(:day_name_friday)) = "friday" THEN 6
-                          WHEN LOWER(TRIM(:day_name_saturday)) = "saturday" THEN 7
-                          ELSE CAST(
-                              REPLACE(
-                                  REPLACE(
-                                      REPLACE(
-                                          LOWER(REPLACE(:day_index_input, "day", "")),
-                                          " ",
-                                          ""
-                                      ),
-                                      "_",
-                                      ""
-                                  ),
-                                  "-",
-                                  ""
-                              ) AS UNSIGNED
-                          )
-                      END
+                      AND day_index = :day_index
                     LIMIT 1
                 ',
 
                 'bindings' => [
-                    'projection_id' => '$context.projection.id',
+                    'projection_id' => '$context.calendar_normalized_input.projection_id',
                     'week_id' => '$context.book_week.id',
-                    'day_name_sunday' => '$input.day_index',
-                    'day_name_monday' => '$input.day_index',
-                    'day_name_tuesday' => '$input.day_index',
-                    'day_name_wednesday' => '$input.day_index',
-                    'day_name_thursday' => '$input.day_index',
-                    'day_name_friday' => '$input.day_index',
-                    'day_name_saturday' => '$input.day_index',
-                    'day_index_input' => '$input.day_index',
+                    'day_index' => '$context.calendar_normalized_input.day_index',
                 ],
             ],
 
@@ -235,23 +176,8 @@ return [
 
             'transition' => [
                 'driver' => 'boolean',
-                'next' => 'await_time_index',
+                'next' => 'validate_book_time',
                 'failure_state' => 'terminal_book_day_not_found',
-            ],
-        ],
-
-        'await_time_index' => [
-            'type' => 'input',
-            'prompt' => 'Which time slot should this event belong to? You can answer with a number or a label such as Time 1.',
-            'expected_input' => 'time_index',
-
-            'transition' => [
-                'driver' => 'match',
-                'value' => '$input.time_index',
-                'cases' => [
-                    '' => 'terminal_missing_time_index',
-                ],
-                'default' => 'validate_book_time',
             ],
         ],
 
@@ -273,28 +199,14 @@ return [
                     FROM calendar_book_times
                     WHERE projection_id = :projection_id
                       AND day_id = :day_id
-                      AND time_index = CAST(
-                          REPLACE(
-                              REPLACE(
-                                  REPLACE(
-                                      LOWER(REPLACE(:time_index_input, "time", "")),
-                                      " ",
-                                      ""
-                                  ),
-                                  "_",
-                                  ""
-                              ),
-                              "-",
-                              ""
-                          ) AS UNSIGNED
-                      )
+                      AND time_index = :time_index
                     LIMIT 1
                 ',
 
                 'bindings' => [
-                    'projection_id' => '$context.projection.id',
+                    'projection_id' => '$context.calendar_normalized_input.projection_id',
                     'day_id' => '$context.book_day.id',
-                    'time_index_input' => '$input.time_index',
+                    'time_index' => '$context.calendar_normalized_input.time_index',
                 ],
             ],
 
@@ -345,7 +257,7 @@ return [
                 'operation' => 'create_book_event',
 
                 'payload' => [
-                    'projection_id' => '$context.projection.id',
+                    'projection_id' => '$context.calendar_normalized_input.projection_id',
                     'book_time_id' => '$context.book_time.id',
                     'event_index' => '$input.event_index',
                     'summary' => '$input.summary',
