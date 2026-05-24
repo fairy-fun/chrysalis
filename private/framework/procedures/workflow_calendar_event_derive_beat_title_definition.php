a74b1efb752a3046826962d81f87d35b0e9262b6
fairy-fun <github@makeyourfairytale.com>
2026-05-24 12:51:34 -0400

    Fix SQL quoting in beat title workflow definition

--- private/framework/procedures/workflow_calendar_event_derive_beat_title_definition.php
+++ private/framework/procedures/workflow_calendar_event_derive_beat_title_definition.php
@@ -32,7 +32,7 @@ return [
                 'operation' => 'select_one',
                 'store' => 'calendar_event',
 
-                'sql' => '
+                'sql' => "
                     SELECT
                         id,
                         entity_id,
@@ -44,9 +44,9 @@ return [
                         sequence_index
                     FROM calendar_events
                     WHERE entity_id = :entity_id
-                       OR entity_id = CONCAT(''calendar_event:'', :entity_id)
+                       OR entity_id = CONCAT('calendar_event:', :entity_id)
                     LIMIT 1
-                ',
+                ",
 
                 'bindings' => [
                     'entity_id' => '$input.calendar_event_entity_id',
