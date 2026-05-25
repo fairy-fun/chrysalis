4345fe40643fae46eaf6241a0297e31bbf550b94
fairy-fun <github@makeyourfairytale.com>
2026-05-24 20:13:55 -0400

    Register suggest characters workflow operation

--- private/framework/procedures/workflow_driver_dispatcher.php
+++ private/framework/procedures/workflow_driver_dispatcher.php
@@ -47,6 +47,7 @@ function fw_execute_workflow_driver_operation(
             'create_book_time' => 'fw_execute_workflow_calendar_book_time_create',
             'process_attached_prose' => 'fw_execute_workflow_calendar_event_process_attached_prose',
             'derive_beat_title' => 'fw_execute_workflow_calendar_event_derive_beat_title',
+            'suggest_characters' => 'fw_execute_workflow_calendar_event_suggest_characters',
             'display_day_prose' => 'fw_execute_workflow_calendar_display_day_prose',
             'display_week_prose' => 'fw_execute_workflow_calendar_display_week_prose',
             'display_week_day_prose' => 'fw_execute_workflow_calendar_display_week_day_prose',
