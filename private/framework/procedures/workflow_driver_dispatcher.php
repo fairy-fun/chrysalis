551723df5f53c6c971e3714aae415c8ea3f7801e
fairy-fun <github@makeyourfairytale.com>
2026-05-25 02:59:44 -0400

    Register prose projection publication workflow operation

--- private/framework/procedures/workflow_driver_dispatcher.php
+++ private/framework/procedures/workflow_driver_dispatcher.php
@@ -38,6 +38,7 @@ function fw_execute_workflow_driver_operation(
             'create_draft' => 'fw_execute_workflow_prose_create_draft',
             'resolve_calendar_event_family' => 'fw_execute_workflow_prose_resolve_calendar_event_family',
             'create_family_draft' => 'fw_execute_workflow_prose_create_family_draft',
+            'set_projection_published_draft' => 'fw_execute_workflow_prose_set_projection_published_draft',
             'segment_subevents' => 'fw_execute_workflow_prose_segment_subevents',
         ],
 
