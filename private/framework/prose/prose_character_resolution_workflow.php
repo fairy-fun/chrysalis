3fe27bea204aea67aedb30210c17537b67307b97
fairy-fun <github@makeyourfairytale.com>
2026-05-26 11:00:49 -0400

    Extract prose character semantic workflow

---
+++ private/framework/prose/prose_character_resolution_workflow.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+require_once __DIR__
+    . '/../entity/entity_resolution_candidate_factory.php';
+
+require_once __DIR__
+    . '/resolution/resolve_token_decomposition.php';
+
+require_once __DIR__
+    . '/../semantic_resolution/semantic_resolution_workflow_runner.php';
+
+function prose_character_resolution_workflow_run(
+    PDO $pdo,
+    string $surfaceForm
+): array {
+
+    return semantic_resolution_workflow_runner_run(
+        $pdo,
+        [
+            [
+                'resolver_stage' =>
+                    'exact_alias',
+
+                'candidates' =>
+                    entity_resolution_candidate_factory_from_surface(
+                        $pdo,
+                        $surfaceForm,
+                        [
+                            'entity_type_id' =>
+                                'entity_type_character',
+
+                            'resolution_method_classval_id' =>
+                                'RESOLUTION_METHOD_EXACT_ALIAS',
+
+                            'resolver_context' =>
+                                __FUNCTION__,
+                        ]
+                    ),
+            ],
+
+            [
+                'resolver_stage' =>
+                    'token_decomposition',
+
+                'candidates' =>
+                    resolve_token_decomposition(
+                        $pdo,
+                        $surfaceForm,
+                        [
+                            'entity_type_id' =>
+                                'entity_type_character',
+
+                            'resolution_method_classval_id' =>
+                                'RESOLUTION_METHOD_TOKEN_DECOMPOSITION',
+
+                            'resolver_stage' =>
+                                'token_decomposition',
+                        ]
+                    ),
+            ],
+        ],
+        [
+            'arbitration_stage' =>
+                'prose_character_resolution',
+        ]
+    );
+}
