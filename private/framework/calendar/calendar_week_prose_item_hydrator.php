ac0a040dd34af0dfb3a547634fff984932a0bd25
fairy-fun <github@makeyourfairytale.com>
2026-05-26 12:22:57 -0400

    calendar_week_prose_item_hydrator.php

--- private/framework/calendar/calendar_week_prose_item_hydrator.php
+++ private/framework/calendar/calendar_week_prose_item_hydrator.php
@@ -82,7 +82,7 @@ function hydrate_calendar_week_prose_item(
             => $row['class_type_label'],
 
         'chronology_address'
-            => (string)($row['chronology_address'] ?? ''),
+            => $row['chronology_address'],
 
         'real_date_start_id'
             => $row['real_date_start_id'],
