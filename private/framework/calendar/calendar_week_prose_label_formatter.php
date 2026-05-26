25c715d8c5f007df592e4cc9ce835842dca454be
fairy-fun <github@makeyourfairytale.com>
2026-05-26 12:30:07 -0400

    calendar_week_prose_label_formatter.php

--- private/framework/calendar/calendar_week_prose_label_formatter.php
+++ private/framework/calendar/calendar_week_prose_label_formatter.php
@@ -9,7 +9,29 @@ function format_calendar_week_prose_label(
     array $event
 ): string {
 
-    $dotNotation = trim((string)($event['chronology_address'] ?? ''));
+    $dotNotation = implode('.', array_filter([
+        (int)($renderTree['week']['week_index'] ?? 0),
+        (int)($day['day_index'] ?? 0),
+        (int)($time['time_index'] ?? 0),
+        $event['event_index'] !== null
+            ? (int)$event['event_index']
+            : null,
+        $event['subevent_index'] !== null
+            ? (int)$event['subevent_index']
+            : null,
+        (
+            $event['subevent_index'] !== null
+            && $event['sequence_index'] !== null
+        )
+            ? (int)$event['sequence_index']
+            : null,
+    ], static fn ($part): bool => $part !== null));
+
+    if ($dotNotation === '') {
+        $dotNotation = trim(
+            (string)($event['chronology_address'] ?? '')
+        );
+    }
 
     $displayReference = trim(
         (string)($event['reference_label'] ?? '')
