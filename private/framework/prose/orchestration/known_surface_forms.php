c19386504c6c077214b66793ab7c351ffd6166bd
fairy-fun <github@makeyourfairytale.com>
2026-05-26 03:57:16 -0400

    Extract known prose surface registry

---
+++ private/framework/prose/orchestration/known_surface_forms.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+function prose_character_known_surface_forms(): array
+{
+    return [
+        [
+            'surface_forms' => [
+                'Shay',
+                'Shay Vertue',
+                'Chloe',
+                'Mr Fruean',
+                'Fruean',
+            ],
+            'confidence' => 0.99,
+        ],
+    ];
+}
