772a6e5bc67c5cdaade8b2652d7d27da2749839a
fairy-fun <github@makeyourfairytale.com>
2026-05-31 12:11:10 -0400

    Register Book time input normalizer

--- private/framework/procedures/workflow_driver_dispatcher.php
+++ private/framework/procedures/workflow_driver_dispatcher.php
@@ -50,6 +50,7 @@ function fw_execute_workflow_driver_operation(
 
         'calendar' => [
             'normalize_book_event_input' => 'fw_execute_workflow_calendar_normalize_book_event_input',
+            'normalize_book_time_input' => 'fw_execute_workflow_calendar_normalize_book_time_input',
             'create_book_event' => 'fw_execute_workflow_calendar_create_book_event',
             'create_book_week' => 'fw_execute_workflow_calendar_book_week_create',
             'create_book_day' => 'fw_execute_workflow_calendar_book_day_create',
