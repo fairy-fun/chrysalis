95870b28a0bef90fd7fe78e891ccbc206ca64c2e
fairy-fun <github@makeyourfairytale.com>
2026-06-01 22:44:04 -0400

    Clean projection rebuild visibility wiring

--- private/framework/contracts/repo_visibility.php
+++ private/framework/contracts/repo_visibility.php
@@ -67,8 +67,6 @@ return [
         'createCalendarEvent' => 'public_html/pecherie/chill-api/calendar/create_calendar_event.php',
         'createCalendarSubevent' => 'public_html/pecherie/chill-api/calendar/create_calendar_subevent.php',
         'getCalendarNodeByChronologyAddress' => 'public_html/pecherie/chill-api/calendar/get_calendar_node_by_chronology_address.php',
-        'tables' => 'public_html/pecherie/chill-api/tables.php',
-        'columns' => 'public_html/pecherie/chill-api/columns.php',
         'listYears' => 'public_html/pecherie/chill-api/reference/list_years.php',
         'listChoreographyTypes' => 'public_html/pecherie/chill-api/reference/list_choreography_types.php',
         'listRoutineStatuses' => 'public_html/pecherie/chill-api/reference/list_routine_statuses.php',
